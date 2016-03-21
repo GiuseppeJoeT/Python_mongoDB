ea27647bf0abe5801bcdbc48623fa77a99db0b98
Giuseppe Tiberi <tiberiweb@gmail.com>
2016-03-21 12:43:52 +0000

    Working with dates in MongoDB

--- mongoDB_test_connection.py
+++ mongoDB_test_connection.py
@@ -9,24 +9,23 @@ def mongo_connect():
     except pymongo.errors.ConnectionFailure, e:
         print "Could not connect to MongoDb: %s" % e
 
+
 # Add a document to the database
 conn = mongo_connect()
 db = conn['twitter_stream']
 coll = db.my_collection
+coll.drop()  # remove the collection
 docs = [{"name": "Code", "surname": "Institute", "twitter": "@codersinstitute"},
-       {"name": "Henry", "surname": "Moore", "twitter": "@henrymoore"},
-       {"name": "Stephen", "surname": "Fry", "twitter": "@stephenfry"}]
+        {"name": "Henry", "surname": "Moore", "twitter": "@henrymoore"},
+        {"name": "Stephen", "surname": "Fry", "twitter": "@stephenfry"}]
 coll.insert_many(docs)
-results = coll.find()
+results = coll.find({"name": "Stephen"})
 
+# Get column and records count
+# print results.count()
+# print coll.count()
+
+# Iterate through the cursor
 for doc in results:
     print doc
-
-'''
-{ u'twitter': u'@codersinstitute',
- u'_id': ObjectId('5629264db1bae125ac446ba5'),
-  u'surname': u'Institute',
-  u'name': u'Code'}
-'''
-
 
